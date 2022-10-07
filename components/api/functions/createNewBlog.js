476fd9b2d821f366931d179596cd9bd5654f078c
kaleab-taye <kaleabtaye@gmail.com>
2022-10-07 16:40:24 +0300

    error fix

--- components/api/functions/createNewBlog.js
+++ components/api/functions/createNewBlog.js
@@ -1,18 +1,21 @@
 import mongoose from 'mongoose';
 import { blogModel } from '../models/blog';
-import {v4 as uuidV4} from 'uuid';
-
+import { v4 as uuidV4 } from 'uuid';
 
 export async function createNewBlog(blog) {
   const mongoDbUrl = process.env.mongoDb_url;
   mongoose.connect(mongoDbUrl);
 
+  let date = new Date();
   try {
-    let response =await blogModel.create({
-        id : uuidV4(),
-        ... blog
+    let newBlog = {
+      id: uuidV4(),
+      ...blog,
+    };
 
-    });
+    newBlog.blogMeta.date = Date.now();
+
+    let response = await blogModel.create(newBlog);
     return response;
   } catch (error) {
     console.log(error);
