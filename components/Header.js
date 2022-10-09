ce001ed611d03ca3db80f62f1f374e2a13cdf97c
kaleab-taye <kaleabtaye@gmail.com>
2022-10-09 23:59:36 +0300

    open graph meta tag

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import icon from '../public/swag-lion.png'
+import icon from '../public/swag-lion.png';
 
-export default function Header( props) {
-  let url = process.env.url
+export default function Header(props) {
+  let url = process.env.url;
   return (
     <Head>
       <title>{props.title}</title>
@@ -10,8 +10,12 @@ export default function Header( props) {
       <link rel="icon" href={props.icon} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:image" content={`${url}/swag-lion.png`} />
-
-      </Head>
+      <meta property="og:title" content={props.title} />
+      <meta property="og:type" content={props.type} />
+      <meta property="og:description" content={props.description} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+    </Head>
   );
 }
 
@@ -19,4 +23,7 @@ Header.defaultProps = {
   title: 'Shuk-Shukta blogs',
   icon: '/favicon.ico',
   content: 'ethiopian blogs, blogging, content writing',
+  type: 'article',
+  description:
+    'A free blogging site where we can share our ideas, get the chance to comment and discus on ideas we would be interested in.',
 };
