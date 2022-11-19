cfd2458fe4d8915c9787502a63f88dff576dca9c
kaleab-taye <kaleabtaye@gmail.com>
2022-11-19 12:47:03 +0300

    blogger card ui improved

--- components/Ui/Blogger-Card.js
+++ components/Ui/Blogger-Card.js
@@ -5,30 +5,46 @@ import Button_comp from './Button_comp';
 
 export default function Blogger_Card({ blogger }) {
   return (
-    <div className="max-w-blogCardWidLg m-auto m-5 p-5 border rounded-lg border-secondary grid grid-flow-col">
+    <div className="max-w-blogCardWidLg m-auto m-5 p-5 border rounded-lg border-secondary grid sm:grid-flow-col">
       <div className="m-auto grid">
         {' '}
         <div className="m-auto inline-block h-40 w-40 rounded-full ring-2 ring-secondary">
           <Image src={heroImage} alt="user image" />
         </div>
-        <div className="mr-auto font-bold text-xl text-textColor1">
+        <div className="mx-auto font-bold text-xl text-textColor1">
           {blogger.fullName}
         </div>
-        <div className="mr-auto text-textColor3 text-sm">
+        <div className="mx-auto text-center text-textColor3 text-sm">
           Joined in {new Date(blogger.joinedDate).toUTCString()}
         </div>
       </div>
-      <div className="grid m">
-        <div className="grid grid-flow-col gap-0 mt-5">
-          <div className="grid grid-rows-3 text-textColor3 text-md gap-2 mb-auto">
-            <div>Email address</div>
-            <div>Username</div>
-            <div>Total posted blogs</div>
-          </div>
-          <div className="grid grid-rows-3 text-textColor1 text-lg font-medium gap-1 mb-auto">
-            <div>{blogger.email}</div>
-            <div>{blogger.userName}</div>
-            <div>{blogger.blogs.length}</div>
+      <div className="grid mx-auto">
+        <div className="grid grid-flow-col gap-1 my-5">
+          <div className="grid  text-textColor3 text-md gap-2 mb-auto">
+            <div className="grid grid-cols-5 gap-2">
+              <div className="col-span-2 my-auto text-textColor3 font-medium text-md">
+                Email address
+              </div>
+              <div className="break-all col-span-3   text-textColor1 font-bold text-lg my-auto">
+                {blogger.email}
+              </div>
+            </div>
+            <div className="grid grid-cols-5 gap-2">
+              <div className="col-span-2 my-auto text-textColor3 font-medium text-md">
+                Username
+              </div>
+              <div className="break-all col-span-3   text-textColor1 font-bold text-lg my-auto">
+                {blogger.userName}
+              </div>
+            </div>
+            <div className="grid grid-cols-5 gap-2">
+              <div className="col-span-2 my-auto text-textColor3 font-medium text-md">
+                Total posted blogs
+              </div>
+              <div className="col-span-3 text-textColor1 font-bold text-lg my-auto">
+                {blogger.blogs.length}
+              </div>
+            </div>
           </div>
         </div>
         <div>
