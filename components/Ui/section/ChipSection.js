9a6b5c9eaebd63efff815bed1e03bcc3490bb6de
kaleab-taye <kaleabtaye@gmail.com>
2022-11-21 16:52:54 +0300

    title added for chips

--- components/Ui/section/ChipSection.js
+++ components/Ui/section/ChipSection.js
@@ -5,10 +5,19 @@ import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 
-export default function ChipSection({ options, selectedSetter=()=>{}, selected=[],disabled=false }) {
+export default function ChipSection({
+  chipTitle,
+  options,
+  selectedSetter = () => {},
+  selected = [],
+  disabled = false,
+}) {
   return (
     <div>
-      <div className="w-full grid gap-2">
+      <div className="w-full grid gap-0">
+        {chipTitle ? (
+          <div className="text-sm text-textColor3 pb-2">{chipTitle}</div>
+        ) : null}
         <div className="flex flex-wrap gap-0 grid-col-auto">
           {options.map((option) => (
             <Chip
@@ -24,7 +33,7 @@ export default function ChipSection({ options, selectedSetter=()=>{}, selected=[
               onClick={() => selectedSetter('')}
               className="flex gap-2 text-sm cursor-pointer w-fit text-center m-1 text-textColor3 border-[1.9px] border-textColor3 rounded-full px-3"
             >
-            <FontAwesomeIcon className="w-2 m-auto" icon={faX} />
+              <FontAwesomeIcon className="w-2 m-auto" icon={faX} />
               <span> clear</span>{' '}
             </div>
           ) : null}
