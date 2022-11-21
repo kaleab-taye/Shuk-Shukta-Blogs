d096512fcce12442f4d045c8dd63b8d5db685e80
kaleab-taye <kaleabtaye@gmail.com>
2022-11-21 16:48:28 +0300

    404 page and component added

--- components/NoBlogAvailable.js
+++ components/NoBlogAvailable.js
@@ -1,14 +1,22 @@
 import PageNotFound from '../pages/404';
-
-export default function NoBlogAvailable() {
+import Image from 'next/image';
+import notFoundImage from './../public/notFound.svg'
+export default function NoBlogAvailable({text}) {
   return (
     <>
       <div className="max-w-contentWid 2xl:max-w-contentWidLg mx-auto grid">
-        <div className="mx-auto text-center grid text-center m-auto text-xl  font-semibold font-commonFont text-onSecondary">
-          <span>No Blog Found!</span>
-          <span className="text-md">You create one now.</span>
+        <div className="mx-auto text-center grid text-center m-auto   font-semibold font-commonFont text-onSecondary">
+          <div className="max-w-[350px]">
+            <Image src={notFoundImage} alt="not found" />
+          </div>
+          <span className='text-xl'>No Blog Found!</span>
+          <span className="text-lg text-textColor3 opacity-80">{text}</span>
         </div>
       </div>
     </>
   );
+}
+
+NoBlogAvailable.defaultProps={
+text:'Go ahead and create one now.'
 }
--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,23 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import {  useRouter } from 'next/router';
+import Button_comp from '../components/Ui/Button_comp';
 import image from '../public/404.png';
 
 export default function PageNotFound() {
+  const router=useRouter()
+  const goHome=()=>{
+    router.replace('/')
+  }
   return (
-    <div className='h-screen flex align-middle'>
-      <div className="max-w-xs sm:max-w-sm m-auto">
-        <Image className="m-20 text-accent " src={image} alt="404 page not found" />
+    <div className="h-screen flex align-middle">
+      <div className="max-w-xs sm:max-w-sm m-auto grid gap-8">
+        <Image
+          className="m-20 text-accent "
+          src={image}
+          alt="404 page not found"
+        />
+        <Button_comp onClick={()=>goHome()}>Go Home</Button_comp>
       </div>
     </div>
   );
