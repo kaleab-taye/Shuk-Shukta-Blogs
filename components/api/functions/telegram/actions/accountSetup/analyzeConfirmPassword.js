310fc5508d9e1045340b55e1fdfde99ed031180c
kaleab-taye <kaleabtaye@gmail.com>
2022-11-28 02:23:02 +0300

    password confirmation added

--- components/api/functions/telegram/actions/accountSetup/analyzeConfirmPassword.js
+++ components/api/functions/telegram/actions/accountSetup/analyzeConfirmPassword.js
@@ -1,3 +1,29 @@
-export default function analyzeConfirmPassword() {
-  return true;
+import { sessionModel } from '../../../../models/session';
+import { telegramChannelModel } from '../../../../models/telegramChannel';
+import sendMessage from '../../components/sendMessage';
+
+export default async function analyzeConfirmPassword(message) {
+  // update data on database
+  const currentSession = await sessionModel.findOne({
+    userId: message.from.id,
+    last: true,
+  });
+  if (currentSession === null) {
+    throw 'session not found';
+  }
+
+  const channel = await telegramChannelModel.findOne({
+    _id: currentSession.channel._id,
+  });
+  if (channel.account.password === message.text) {
+    const resp = await fetch(
+      sendMessage({
+        chat_id: message.from.id,
+        text: `Your data is set as the following %0a%0aBlogger Name ${channel.account.name} %0aBlogger Username ${channel.account.username} %0aEmail ${channel.account.email} %0a%0ayou can go to our website and edit this data anytime.`,
+      })
+    );
+    return true;
+  } else {
+    return false;
+  }
 }
